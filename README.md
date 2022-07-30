03a1b788c29ac896e6d6d3e10ebf48887c2bb113
Ryan Hagelstrom <rhagelstrom@gmail.com>
2022-07-30 18:49:41 -0500

    Initial Release

---
+++ README.md
@@ -0,0 +1,6 @@
+# Power Up
+
+**Current Version:** 1.0
+**Updated::** 07/30/22
+
+Power Up is Fantasy Grounds extension that allows a user to see the name of extensions that have been updated since the last time the campaign was loaded. Use the following slash command to show which extensions have been updated:  /powerup
---
+++ scripts/PowerUp.lua
@@ -0,0 +1,54 @@
+
+--  	Author: Ryan Hagelstrom
+--	  	Copyright © 2022
+--	  	This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+--	  	https://creativecommons.org/licenses/by-sa/4.0/
+
+local tExtensions = {}
+
+function onInit()
+	Comm.registerSlashHandler("powerup", powerUp);
+	if  Session.IsHost then
+		DB.createNode("PowerUp");
+	end
+	for _,sName in pairs(Extension.getExtensions()) do
+		local tInfo = Extension.getExtensionInfo(sName)
+		tExtensions[tInfo.name] = tInfo.version
+   	end
+end
+
+function onClose()
+	if  Session.IsHost then
+		local nodeDB  = DB.findNode("PowerUp");
+		if nodeDB then
+			for sName, sVersion in pairs(tExtensions) do
+				DB.setValue(nodeDB, UtilityManager.encodeXML(sName):gsub("%s","_"):gsub(":",""), "string", sVersion)
+			end
+		end
+	end
+end
+
+function powerUp()
+	local bNoUpdates = true
+	local sMessage = {font = "systemfont"};
+	sMessage.icon = "PowerUpChat"
+	local nodeDB  = DB.findNode("PowerUp");
+	if nodeDB then
+		for sName, sVersion in pairs(tExtensions) do
+			local sOldVersion = DB.getValue(nodeDB, UtilityManager.encodeXML(sName):gsub("%s","_"):gsub(":",""), '')
+			if sOldVersion == '' then
+				sMessage.text = sName .. " new to campaign";
+				Comm.addChatMessage(sMessage);
+				bNoUpdates = false;
+			elseif sVersion ~= sOldVersion then
+				sMessage.text = sName .. " Old: " .. sOldVersion .. " New: ".. sVersion;
+				Comm.addChatMessage(sMessage);
+				bNoUpdates = false;
+			end
+		end
+	end
+	if bNoUpdates then
+		sMessage.text = "No extension updates detected";
+		Comm.addChatMessage(sMessage);
+	end
+end
